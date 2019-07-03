75c75cd690f5c3bf5deb4285ab093f469dcbca60
training_d2.03.07 <training_d2.03.07@DDC2-D-772R9B2.dir.svc.accenture.com>
2019-07-03 10:48:37 +0530

    json file

--- src/test/java/cucumber/calcrunner.java
+++ src/test/java/cucumber/calcrunner.java
@@ -2,9 +2,11 @@ package cucumber;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin= {"json:cucumber.json"})
 public class calcrunner {
 
 }
